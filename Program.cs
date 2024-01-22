3bb05a70e6d96160fece2b771764448b1e81b625
Nicholas-Fultz-student <144163154+Nicholas-Fultz-student@users.noreply.github.com>
2024-01-22 19:19:03 +0000

    added basic info cases

--- Program.cs
+++ Program.cs
@@ -1,9 +1,54 @@
-﻿class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class Program
 {
 
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-
+        System.Console.WriteLine("WELCOME MESSAGE");
+        System.Console.WriteLine("ENTER INFO, NAME, BOSSES, QUIT");
+        string userInput = Console.ReadLine();
+        
+        while (userInput.ToLower() != "quit")
+        {
+          userChoices(userInput);  
+          userInput = Console.ReadLine();
+        }
+        
         
     }
+
+    static void userChoices(string userInput)
+    {
+        switch (userInput.ToLower())
+        {
+
+            case "info":
+                System.Console.WriteLine("INFORMATION TEXT");
+                break;
+
+            case "name":
+                System.Console.WriteLine("RETURNING NAME TEXT");
+                break;
+
+            case "bosses":
+                System.Console.WriteLine("BOSS CHOICES");
+                break;
+
+            default:
+                System.Console.WriteLine("NOT VALID INPUT RESPONSE");
+                break;
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
 }
