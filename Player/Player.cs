f901ae5f60651b8e33a0e4fa1a9722de137b6460
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2024-01-22 19:43:31 +0000

    added weapon to player class

--- Player/Player.cs
+++ Player/Player.cs
@@ -2,12 +2,16 @@ public class Player {
 
     private int health;
     private string name;
+    private string weapon;
     private string[] inventory = new string[4];
 
-    public Player(string name, string startingPotion) {
+    public Player(string name, string startingPotion, string weapon) {
         this.name = name;
+        this.weapon = weapon;
         health = 100;
-        inventory[0] = startingPotion;
+        
+        inventory[0] = weapon;
+        inventory[1] = startingPotion;
     }
 
     public void SetName(string newName) {
