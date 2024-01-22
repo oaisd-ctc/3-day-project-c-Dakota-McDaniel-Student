4f51d38797f444a81b88f8c915a4ec3e90530912
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2024-01-22 19:22:22 +0000

    Created Player class

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,4 +1,25 @@
 public class Player {
-    
+
+    private int health;
+    private string name;
+    private string[] inventory;
+
+    public Player(string name, string startingPotion) {
+        this.name = name;
+        health = 100;
+        inventory[0] = startingPotion;
+    }
+
+    public void SetName(string newName) {
+        name = newName;
+    }
+
+    public int GetHealth() {
+        return health;
+    }
+
+    public string GetInventory() {
+        return inventory[0];
+    }
 
 }
